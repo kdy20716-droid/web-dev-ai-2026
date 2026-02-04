10412b72b29ad5589428ec5d2b97d0b9eb7a23be
kdy20716-droid <kdy20716@gmail.com>
2026-02-04 15:41:29 +0900

    //

--- 03-javascript/practice05-function-updown.js
+++ 03-javascript/practice05-function-updown.js
@@ -6,6 +6,7 @@ function gameStart() {
   while (true) {
     let input = prompt("1부터 100 사이의 숫자를 입력해보세요!");
 
+    // 무한반복 -> 종료하는 시점 (break)
     // 취소 버튼을 눌렀을 때 input이 null한테 이때 종료
     if (input === null) {
       alert("게임을 종료합니다.");
@@ -18,6 +19,7 @@ function gameStart() {
     }
 
     let myNumber = Number(input);
+    // 끝나는 시점! 내가 입력한 input 값이 랜덤 값을 맞춤
 
     count = count + 1;
 
