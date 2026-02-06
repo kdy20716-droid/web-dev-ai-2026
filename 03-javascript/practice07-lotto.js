fe91e1643c7b8f61a4bfde6db43e236655e69d1e
kdy20716-droid <kdy20716@gmail.com>
2026-02-06 13:34:40 +0900

    practice07

---
+++ 03-javascript/practice07-lotto.js
@@ -0,0 +1,88 @@
+const lotto = [];
+while (true) {
+  const num = Math.floor(Math.random() * 45) + 1;
+  if (!lotto.includes(num)) {
+    lotto.push(num);
+  }
+  if (lotto.length === 6) {
+    break;
+  }
+}
+const bonus = [];
+while (true) {
+  const num = Math.floor(Math.random() * 45) + 1;
+  if (!bonus.includes(num)) {
+    bonus.push(num);
+  }
+  if (bonus.length === 1) {
+    break;
+  }
+}
+// 로또 번호
+console.log("로또번호", lotto, bonus);
+
+// 내가 입력한 번호도 6개 숫자 랜덤으로 맞출 때까지
+const myLotto = [];
+while (true) {
+  const num = Math.floor(Math.random() * 45) + 1;
+  if (!myLotto.includes(num)) {
+    myLotto.push(num);
+  }
+  if (myLotto.length === 6) {
+    break;
+  }
+}
+console.log(myLotto);
+// 1등 당첨 : 6개 일치
+const count = lotto.filter((num) => myLotto.includes(num)).length;
+if (count === 6) {
+  console.log("1등 당첨!");
+}
+// 2등 당첨 : 5개 일치 + 보너스
+if (count === 5 && bonus.includes(myLotto[6])) {
+  console.log("2등 당첨!");
+}
+// 3등 당첨 : 5개 일치
+if (count === 5) {
+  console.log("3등 당첨!");
+}
+// 4등 당첨 : 4개 일치
+if (count === 4) {
+  console.log("4등 당첨!");
+}
+// 5등 당첨 : 3개 일치
+if (count === 3) {
+  console.log("5등 당첨!");
+}
+// 테스트 : 1~5등 당첨 될때까지 무한반복 (ai사용)
+let tryCount = 0;
+const intervalId = setInterval(() => {
+  tryCount++;
+  const myTestLotto = [];
+  while (myTestLotto.length < 6) {
+    const num = Math.floor(Math.random() * 45) + 1;
+    if (!myTestLotto.includes(num)) {
+      myTestLotto.push(num);
+    }
+  }
+
+  const matchCount = lotto.filter((num) => myTestLotto.includes(num)).length;
+  console.log(myTestLotto);
+
+  if (matchCount === 6) {
+    console.log("1등 당첨!");
+    clearInterval(intervalId);
+  } else if (matchCount === 5 && myTestLotto.includes(bonus[0])) {
+    console.log("2등 당첨!");
+    clearInterval(intervalId);
+  } else if (matchCount === 5) {
+    console.log("3등 당첨!");
+    clearInterval(intervalId);
+  } else if (matchCount === 4) {
+    console.log("4등 당첨!");
+    clearInterval(intervalId);
+  } else if (matchCount === 3) {
+    console.log("5등 당첨!");
+    clearInterval(intervalId);
+  }
+}, 100);
