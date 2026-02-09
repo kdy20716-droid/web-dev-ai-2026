b95e91edd1af33546bc388ba09041d9ae462df5b
kdy20716-droid <kdy20716@gmail.com>
2026-02-09 15:40:30 +0900

    practice08

---
+++ 03-javascript/practice08-dom.js
@@ -0,0 +1,70 @@
+const quotes = [
+  {
+    en: "God doesn't require us to succeed: he only requires that you try.",
+    ko: "신은 우리에게 성공을 요구하지 않는다. 우리가 노력할 것을 요구할 뿐이다.",
+  },
+  {
+    en: "Hold faithfulness and sincerity as first principles.",
+    ko: "충심과 성실을 첫 번째 원칙으로 삼아라.",
+  },
+  {
+    en: "Only actions give life strength; only moderation gives it a charm.",
+    ko: "행동만이 삶에 힘을 주고 절제만이 삶에 매력을 준다.",
+  },
+  {
+    en: "No one has ever made a difference by being like everyone else.",
+    ko: "그저 남들과 똑같이 살면서 차이를 만들어낸 사람은 없다.",
+  },
+];
+const dateElement = document.querySelector("#date");
+const timeElement = document.querySelector("#time");
+const remainingElement = document.querySelector("#remaining");
+const quoteElement = document.querySelector("#quote");
+
+const now = new Date();
+// 숫자를 두 자리로 만들 때 사용
+const minute = String(now.getMinutes()).padStart(2, "0");
+function updateClock() {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const date = now.getDate();
+  const days = ["일", "월", "화", "수", "목", "금", "토"];
+  const day = days[now.getDay()];
+
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  dateElement.textContent = `${year}년 ${month}월 ${date}일 (${day}요일)`;
+  timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+
+  // 올해 남은 시간 계산
+  const endOfYear = new Date(year, 11, 31, 23, 59, 59);
+  const diff = endOfYear - now;
+
+  const rDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const rHours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const rMinutes = Math.floor((diff / (1000 * 60)) % 60);
+  const rSeconds = Math.floor((diff / 1000) % 60);
+
+  remainingElement.textContent = `올해 남은 시간: ${rDays}일 ${rHours}시간 ${rMinutes}분 ${rSeconds}초`;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
+
+// 명언 3초마다 랜덤으로 출력
+let currentQuoteIndex = -1;
+function updateQuote() {
+  let randomIndex = Math.floor(Math.random() * quotes.length);
+  while (randomIndex === currentQuoteIndex) {
+    randomIndex = Math.floor(Math.random() * quotes.length);
+  }
+  currentQuoteIndex = randomIndex;
+  const randomQuote = quotes[randomIndex];
+  quoteElement.innerHTML = `<div class="en">${randomQuote.en}</div><div class="ko">${randomQuote.ko}</div>`;
+}
+updateQuote();
+setInterval(updateQuote, 3000);
