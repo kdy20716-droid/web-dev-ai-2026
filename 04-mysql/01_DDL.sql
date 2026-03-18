813d60d45d5c594d4da38774773e472fce17964c
kdy20716-droid <kdy20716@gmail.com>
2026-03-18 19:44:16 +0900

    save

---
+++ 04-mysql/01_DDL.sql
@@ -0,0 +1,12 @@
+/* 여러줄 주석 */
+-- 한줄 주석
+/*
+    - 데이터(data) - 화면에 보여지거나, 사용자가 입력하거나, 저장해야 하는 정보
+    - 데이터베이스(database) : 대이터를 저장하고 필요할 때 꺼내 쓰는 공간
+    - DBMS(database management system) : 데이터베이스를 관리하는 프로그램
+    - RDBMS(relational database management system) : 관계형 데이터베이스 관리 시스템
+       예) MYSQL, ORACLE, PostgreSQL
+
+    - 엔티티(Entity) : 같은 성격을 가진 데이터를 묶어놓는 큰 주제
+    
+*/
